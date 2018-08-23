2098081ecf4ecaa0ed01e1215608624882e1afef
Fred <domaine75@gmail.com>
2018-08-23 19:42:26 +0200

    add unit tests

--- ConsoleApp472/Program.cs
+++ ConsoleApp472/Program.cs
@@ -4,14 +4,19 @@ namespace ConsoleApp472
 {
    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            for (int  i = 0;  i < 20 ;  i++)
+            for (int  i = 0;  i < 60 ;  i++)
             {
                 if (IsPrime(i))
                 {
                     Console.WriteLine(i);
                 }
+            }
+
+            if (IsPrime(int.MaxValue))
+            {
+                Console.WriteLine($"int.MaxValue is prime: {int.MaxValue}");
             }
 
             Console.WriteLine("press a key to exit:");
@@ -25,7 +30,22 @@ namespace ConsoleApp472
             if (number == 2 || number == 3 || number == 5) return true;
             if (number % 2 == 0) return false;
             if (number % 5 == 0) return false;
-            for (int i = 3; i < Math.Abs(Math.Sqrt(i)); i = i + 2)
+            for (int i = 3; i <= Math.Abs(Math.Sqrt(number)); i = i + 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsPrime(long number)
+        {
+            // process false numbers first
+            if (number < 2) return false;
+            if (number == 2 || number == 3 || number == 5) return true;
+            if (number % 2 == 0) return false;
+            if (number % 5 == 0) return false;
+            for (long i = 3; i <= Math.Abs(Math.Sqrt(number)); i = i + 2)
             {
                 if (number % i == 0) return false;
             }
--- UnitTestApplication/UnitTestApplication.cs
+++ UnitTestApplication/UnitTestApplication.cs
@@ -82,7 +82,7 @@ namespace UnitTestApplication
         public void TestMethod_Prime_9()
         {
             const int source = 9;
-            const bool expected = true;
+            const bool expected = false;
             bool result = Program.IsPrime(source);
             Assert.AreEqual(result, expected);
         }
@@ -133,7 +133,16 @@ namespace UnitTestApplication
         }
 
         [TestMethod]
-        public void TestMethodPrime_23()
+        public void TestMethod_Prime_21()
+        {
+            const int source = 21;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_23()
         {
             const int source = 23;
             const bool expected = true;
@@ -142,9 +151,101 @@ namespace UnitTestApplication
         }
 
         [TestMethod]
-        public void TestMethodPrime_29()
+        public void TestMethod_Prime_29()
         {
             const int source = 29;
+            const bool expected = true;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_31()
+        {
+            const int source = 31;
+            const bool expected = true;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_37()
+        {
+            const int source = 37;
+            const bool expected = true;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        // testing large numbers
+        [TestMethod]
+        public void TestMethod_Prime_123()
+        {
+            const int source = 123;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_127()
+        {
+            const int source = 127;
+            const bool expected = true;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_1234567()
+        {
+            const int source = 1234567;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+
+        [TestMethod]
+        public void TestMethod_Prime_123456789()
+        {
+            const int source = 123456789;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_104729()
+        {
+            const int source = 104729;
+            const bool expected = true;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_1004729()
+        {
+            const int source = 1004729;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_10004729()
+        {
+            const int source = 10004729;
+            const bool expected = false;
+            bool result = Program.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_int_max_value()
+        {
+            const int source = int.MaxValue;
             const bool expected = true;
             bool result = Program.IsPrime(source);
             Assert.AreEqual(result, expected);
