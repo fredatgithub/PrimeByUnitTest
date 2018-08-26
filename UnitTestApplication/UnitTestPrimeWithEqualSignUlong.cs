fb36f241b8b82898e717433af6e14622c1567c8f
Fred <domaine75@gmail.com>
2018-08-27 00:30:26 +0200

    Add unit tests for prime with equal sign

---
+++ UnitTestApplication/UnitTestPrimeWithEqualSignUlong.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp472;
+
+namespace UnitTestApplication
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestMethod_Prime_Ulong_max_value()
+        {
+            const ulong source = ulong.MaxValue;
+            const bool expected = false;
+            bool result = Program.IsPrimeWithEgalSign(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_Ulong_3_times_int_max_value()
+        {
+            const ulong source = (ulong)int.MaxValue * 3;
+            const bool expected = false;
+            bool result = Program.IsPrimeWithEgalSign(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_Ulong_7_times_int_max_value()
+        {
+            const ulong source = (ulong)int.MaxValue * 7;
+            const bool expected = false;
+            bool result = Program.IsPrimeWithEgalSign(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_Prime_long_1000000000100011()
+        {
+            const ulong source = 1000000000100011;
+            const bool expected = true;
+            bool result = Program.IsPrimeWithEgalSign(source);
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
